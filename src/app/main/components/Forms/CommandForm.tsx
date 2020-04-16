61c02c8ac86580b804b35f4f93e81ba16b9d494a
Cameron Brouk <cameronbrouk@gmail.com>
2020-04-15 20:55:15 -0500

    COMMANDS: added systems hook to form

--- src/app/main/components/Forms/CommandForm.tsx
+++ src/app/main/components/Forms/CommandForm.tsx
@@ -3,8 +3,13 @@ import styled from 'styled-components'
 import { Form, Field } from 'react-final-form'
 import { MatTextField } from '../../../shared/components/final-form'
 import { Button } from '@material-ui/core'
+import { useSystems } from '../../hooks'
+import { useObservable } from 'rxjs-hooks'
+import { pluck } from 'rxjs/operators'
 
 const CommandForm = ({ ...props }: any) => {
+  const { getSystems$ } = useSystems()
+  const systems = useObservable(() => getSystems$().pipe(pluck('name')))
   const handleSubmit = (formData: any) => {}
 
   return (
