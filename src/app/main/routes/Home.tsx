666929313bd2132345a1a55e342cea11eb8e7c79
Cameron Brouk <cameronbrouk@gmail.com>
2020-04-16 01:40:29 -0500

    HOME:  added commands table

--- src/app/main/routes/Home.tsx
+++ src/app/main/routes/Home.tsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import styled from 'styled-components'
 import { SystemForm, CommandForm } from '../components'
-import { useSystems } from '../hooks'
+import { useSystems, useCommands } from '../hooks'
 import { useObservable } from 'rxjs-hooks'
 
 import * as Model from '../models'
+import CommandsTable from '../components/Commands/CommandsTable'
 
 const Home = ({ className }: any) => {
   const { getSystems$ } = useSystems()
+  const { getCommands$ } = useCommands()
+
   const systems = useObservable(() => getSystems$<Model.System>())
   console.log(systems)
+
+  const commands = useObservable(() => getCommands$<Model.Command>(), [])
+  console.log(commands)
+
   return (
     <section className={className}>
-      <SystemForm />
+      {/* <SystemForm /> */}
+      <CommandsTable commands={commands} />
     </section>
   )
 }
