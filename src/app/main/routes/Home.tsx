e43ea7a2deaaeff09387046cc081aa577be08395
Cameron Brouk <cameronbrouk@gmail.com>
2020-04-15 20:27:26 -0500

    HOME: added SystemForm to home page

--- src/app/main/routes/Home.tsx
+++ src/app/main/routes/Home.tsx
@@ -1,40 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
+import { SystemForm, CommandForm } from '../components'
 
 const Home = ({ className }: any) => {
   return (
     <section className={className}>
-      <div className='home-overlay'></div>
-      <img
-        className='home-image'
-        src={require('../../../assets/meeting.jpg')}
-        alt='home page'
-      />
-      {/*  */}
-      {/*  */}
-      {/*  */}
-      {/*  */}
+      <SystemForm />
     </section>
   )
 }
 
-export default styled(Home)`
-  .home-overlay,
-  .home-image {
-    position: absolute;
-    margin-top: -10vh;
-    width: 100vw;
-    height: 100vh;
-    max-width: 100%;
-    object-fit: cover;
-  }
-
-  .home-image {
-    z-index: -2;
-  }
-
-  .home-overlay {
-    background-color: rgba(0, 0, 0, 0.7);
-    z-index: -1;
-  }
-`
+export default styled(Home)``
