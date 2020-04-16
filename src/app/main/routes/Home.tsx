91bae6519b769d4b687b3c689525e3276d86ce96
Cameron Brouk <cameronbrouk@gmail.com>
2020-04-16 00:28:46 -0500

    HOME: added generic type to getSystem function

--- src/app/main/routes/Home.tsx
+++ src/app/main/routes/Home.tsx
@@ -4,9 +4,11 @@ import { SystemForm, CommandForm } from '../components'
 import { useSystems } from '../hooks'
 import { useObservable } from 'rxjs-hooks'
 
+import * as Model from '../models'
+
 const Home = ({ className }: any) => {
   const { getSystems$ } = useSystems()
-  const systems = useObservable(() => getSystems$())
+  const systems = useObservable(() => getSystems$<Model.System>())
   console.log(systems)
   return (
     <section className={className}>
