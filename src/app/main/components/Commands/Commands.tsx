0fd8d8615893be2826a618e52247e46b7fb4018d
Cameron Brouk <cameronbrouk@gmail.com>
2020-04-16 12:45:12 -0500

    COMMAND: added table for quick editing

--- src/app/main/components/Commands/Commands.tsx
+++ src/app/main/components/Commands/Commands.tsx
@@ -3,11 +3,9 @@ import MaterialTable, { Column } from 'material-table'
 import { useCommands } from '../../hooks'
 
 import * as Model from '../../models'
-import { componentOverrides } from './table.styles'
 import { useObservable } from 'rxjs-hooks'
 import { map } from 'rxjs/operators'
 import SystemForm from '../Forms/SystemForm'
-import { createMuiTheme, MuiThemeProvider } from '@material-ui/core'
 
 interface Props {
   commands: Model.Command[]
@@ -17,40 +15,16 @@ interface Props {
 const CommandsTable = (props: Props) => {
   const { updateCommand, deleteCommand, createCommand } = useCommands()
 
-  const style = {
-    backgroundColor: '#2e1534',
-    color: 'white',
-  }
-
-  const theme = createMuiTheme({
-    palette: {
-      primary: {
-        main: '#eee',
-      },
-      secondary: {
-        main: '#ff9100',
-      },
-    },
-  })
-
   const columns: Array<Column<Model.Command>> = [
     { title: 'Name', field: 'name' },
     { title: 'Description', field: 'description' },
     { title: 'Category', field: 'category' },
   ]
-  // .map(column => ({ ...column, cellStyle: style, headerStyle: style }))
-
-  const options = {
-    // rowStyle: style,
-    // headerStyle: style,
-  }
 
   return (
-    // <MuiThemeProvider theme={theme}>
     <MaterialTable
       title='Edit A Document'
       columns={columns}
-      options={options}
       data={props.commands}
       editable={{
         onRowAdd: newData =>
@@ -62,9 +36,7 @@ const CommandsTable = (props: Props) => {
         onRowUpdate: (newData, oldData) => updateCommand(newData.id, newData),
         onRowDelete: oldData => deleteCommand(oldData.id),
       }}
-      // components={componentOverrides}
     />
-    // </MuiThemeProvider>
   )
 }
 
