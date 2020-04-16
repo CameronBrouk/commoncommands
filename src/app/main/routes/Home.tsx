2b9b6fbebb7f451b660f8476e0fe69c715ebd229
Cameron Brouk <cameronbrouk@gmail.com>
2020-04-15 20:41:30 -0500

    Home: added getSystem function to home

--- src/app/main/routes/Home.tsx
+++ src/app/main/routes/Home.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
 import { SystemForm, CommandForm } from '../components'
+import { useSystems } from '../hooks'
+import { useObservable } from 'rxjs-hooks'
 
 const Home = ({ className }: any) => {
+  const { getSystems$ } = useSystems()
+  const systems = useObservable(() => getSystems$())
+  console.log(systems)
   return (
     <section className={className}>
       <SystemForm />
