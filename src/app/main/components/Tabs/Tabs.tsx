6b944da6e5604cafc6c781efdc13a0a584223ae3
Cameron Brouk <cameronbrouk@gmail.com>
2020-04-18 22:29:04 -0500

    MAIN: refactor home to contain tabs

--- src/app/main/components/Tabs/Tabs.tsx
+++ src/app/main/components/Tabs/Tabs.tsx
@@ -1,40 +1,25 @@
 import React from 'react'
-import { Tabs as MatTabs, Tab as MatTab, Typography } from '@material-ui/core'
+import { Tabs as MatTabs } from '@material-ui/core'
+import { withStyles } from '@material-ui/core/styles'
 
-import * as Style from './tabs.styles'
+export const StyledTabs = withStyles({
+  indicator: {
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+    '& > div': {
+      maxWidth: 40,
+      width: '100%',
+      backgroundColor: '#635ee7',
+    },
+  },
+})
 
-interface TabsProps {
+interface Props {
   value: number
   onChange: (event: React.ChangeEvent<{}>, newValue: number) => void
 }
-export const Tabs = Style.StyledTabs((props: TabsProps) => (
+
+export default StyledTabs((props: Props) => (
   <MatTabs {...props} centered TabIndicatorProps={{ children: <div /> }} />
 ))
-
-interface TabProps {
-  label: string
-}
-export const Tab = Style.StyledTab((props: TabProps) => (
-  <MatTab disableRipple {...props} />
-))
-
-interface TabPanelProps {
-  children?: React.ReactNode
-  index: any
-  value: any
-}
-export const TabPanel = (props: TabPanelProps) => {
-  const { children, value, index, ...other } = props
-
-  return (
-    <Typography
-      component='div'
-      role='tabpanel'
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}>
-      {children}
-    </Typography>
-  )
-}
--- src/app/main/routes/Home.tsx
+++ src/app/main/routes/Home.tsx
@@ -1,24 +1,64 @@
-import React from 'react'
-import styled from 'styled-components'
-import { useSystems, useCommands } from '../hooks'
+import React, { useState } from 'react'
 import { useObservable } from 'rxjs-hooks'
+import { combineLatest } from 'rxjs'
+
+import { useSystems, useCommands } from '../hooks'
+import { Tabs, Tab, TabPanel } from '../components'
+import { materialClasses } from '../main.styles'
 
 import * as Model from '../models'
-import SystemTabs from '../components/Tabs/SystemTabs'
+import * as Helper from '../helpers'
 
-const Home = ({ className }: any) => {
-  const [value, setValue] = React.useState(0)
+import Commands from '../components/Commands/Commands'
+import { useUI } from '../../shared/hooks/ui.hooks'
+
+const Home = () => {
+  const { tabs, home, desktopPanel } = materialClasses()
+
+  const { isMobile } = useUI()
+
+  const [currentTabIndex, setCurrentTabIndex] = useState(0)
+
   const { getSystems$ } = useSystems()
   const { getCommands$ } = useCommands()
 
-  const systems = useObservable(() => getSystems$<Model.System>())
-  const commands = useObservable(() => getCommands$<Model.Command>(), [])
+  const [systems, commands] = useObservable(() => {
+    const systems$ = getSystems$<Model.System>()
+    const commands$ = getCommands$<Model.Command>()
+    return combineLatest(systems$, commands$)
+  }, [[], []])
+
+  const handleChange = (event: any, newValue: number) => {
+    setCurrentTabIndex(newValue)
+  }
 
   return (
-    <section className={className}>
-      <SystemTabs />
+    <section className={home}>
+      {systems && commands && (
+        <div className={tabs}>
+          <Tabs
+            value={currentTabIndex}
+            onChange={handleChange}
+            aria-label='choose which systems commands to show'>
+            {systems.map(system => (
+              <Tab key={system.id} label={system.name} />
+            ))}
+          </Tabs>
+
+          {systems.map((system, i) => (
+            <aside className={!isMobile ? desktopPanel : ''}>
+              <TabPanel value={currentTabIndex} index={i} key={i}>
+                <Commands
+                  commands={Helper.filterCommandsBySystem(commands, system)}
+                  system={system}
+                />
+              </TabPanel>
+            </aside>
+          ))}
+        </div>
+      )}
     </section>
   )
 }
 
-export default styled(Home)``
+export default Home
