dd9be4f19eab1720aa691f0bea921f6d8c297ed9
Cameron Brouk <cameronbrouk@gmail.com>
2020-04-18 22:47:49 -0500

    APP: remove user route

--- src/app/App.routes.tsx
+++ src/app/App.routes.tsx
@@ -1,16 +1,11 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import User from './user/User.routes'
-// import { Home, About, Team } from './main/routes'
 import { Login, Register } from './user/routes'
 import { Home } from './main/routes'
 
 export default function AppRoutes() {
   return (
     <Switch>
-      <Route path='/users'>
-        <User />
-      </Route>
       <Route path='/login'>
         <Login />
       </Route>
