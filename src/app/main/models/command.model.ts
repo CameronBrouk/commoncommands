b40a706b3ad3cce295e82e68170563550c4ecb2e
Cameron Brouk <cameronbrouk@gmail.com>
2020-04-18 22:07:57 -0500

    COMMAND: make description attribute optional

--- src/app/main/models/command.model.ts
+++ src/app/main/models/command.model.ts
@@ -2,6 +2,8 @@ export interface Command {
   id: string
   systemRef: string
   name: string
-  description: string
+  description?: string
   category: string
 }
+
+export type views = 'category' | 'search' | 'edit'
