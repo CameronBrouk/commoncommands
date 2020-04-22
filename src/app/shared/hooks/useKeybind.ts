1fc7827e0e9ef766743d6e5ec3e8ed80f513de7c
Cameron Brouk <cameronbrouk@gmail.com>
2020-04-21 19:06:09 -0500

    extend keycode and add optional prop to useKeybind

--- src/app/shared/hooks/useKeybind.ts
+++ src/app/shared/hooks/useKeybind.ts
@@ -1,11 +1,13 @@
 import { useEffect } from 'react'
 import { fromEvent } from 'rxjs'
-import { tap, filter, pluck } from 'rxjs/operators'
+import { filter, tap, pluck } from 'rxjs/operators'
 
+// This List is Not Exhaustive, but Native Keyboard Event
+// does not display a specific type
 // prettier-ignore
-type keycode = 'Escape' | 'Enter' | 'Control' | 'Alt' | 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z'
+type keycode = 'Escape' | 'Enter' | 'Control' | 'Alt' | 'ArrowLeft' | 'ArrowRight' | 'ArrowDown' | 'ArrowUp' | 'Tab' | 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z'
 
-export const useKeybind = (key: keycode, onPress: () => any) => {
+export const useKeybind = (key: keycode, onPress: () => void) => {
   const keyPress$ = fromEvent<KeyboardEvent>(document, 'keydown').pipe(
     tap(event => event.preventDefault()),
     pluck('key'),
