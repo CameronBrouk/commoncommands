21b13eb5aa4832ad2105dd7c683e95c8b1f9007a
Cameron Brouk <cameronbrouk@gmail.com>
2020-05-28 21:01:56 -0500

    remove commented out code

--- src/app/shared/hooks/ui.hooks.tsx
+++ src/app/shared/hooks/ui.hooks.tsx
@@ -1,12 +1,8 @@
-import React from 'react'
 import { fromEvent } from 'rxjs'
 import { debounceTime, map } from 'rxjs/operators'
 import { useObservable } from 'rxjs-hooks'
-import { VariantType, useSnackbar } from 'notistack'
 
 export const useUI = () => {
-  // const { enqueueSnackbar } = useSnackbar()
-
   const isMobile = useObservable(
     () =>
       fromEvent(window, 'resize').pipe(
@@ -16,21 +12,7 @@ export const useUI = () => {
     window.innerWidth <= 750,
   )
 
-  // const showSnackbar = (variant: VariantType) => (message: string) =>
-  //   enqueueSnackbar(message, { variant })
-
-  // const showDefault = showSnackbar('default')
-  // const showSuccess = showSnackbar('success')
-  // const showError = showSnackbar('error')
-  // const showWarning = showSnackbar('warning')
-  // const showInfo = showSnackbar('info')
-
   return {
     isMobile,
-    // showDefault,
-    // showSuccess,
-    // showError,
-    // showWarning,
-    // showInfo,
   }
 }
