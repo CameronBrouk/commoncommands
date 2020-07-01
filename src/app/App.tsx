2e79ebfbc702267a8d4fef5a49d4a53587f29c92
Cameron Brouk <cameronbrouk@gmail.com>
2020-07-01 17:25:31 -0500

    fix open/close mobile sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -11,6 +11,7 @@ import AppProvider from './App.context'
 const App = () => {
   const { isMobile } = useUI()
   const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+  // console.log(isSidebarOpen)
 
   return (
     <>
@@ -20,37 +21,35 @@ const App = () => {
           <div className='h-screen flex overflow-hidden bg-gray-100'>
             {/* <!-- Off-canvas menu for mobile --> */}
 
-            {isMobile && (
-              <MobileSidebar
-                isOpen={isSidebarOpen}
-                closeSidebar={() => setIsSidebarOpen(false)}
-              />
-            )}
+            <MobileSidebar
+              isOpen={isSidebarOpen}
+              closeSidebar={() => setIsSidebarOpen(false)}
+            />
 
             {!isMobile && <DesktopSidebar />}
 
             <div className='flex flex-col w-0 flex-1 overflow-hidden'>
-              {/* <div className='md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3'>
+              <div className='relative z-10 flex-shrink-0 flex h-16 bg-white shadow'>
                 <button
-                  className='-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:bg-gray-200 transition ease-in-out duration-150'
+                  className='px-4 border-r border-gray-200 text-gray-500 focus:outline-none focus:bg-gray-100 focus:text-gray-600 md:hidden'
                   aria-label='Open sidebar'
                   onClick={() => setIsSidebarOpen(true)}>
                   <svg
                     className='h-6 w-6'
+                    stroke='currentColor'
                     fill='none'
-                    viewBox='0 0 24 24'
-                    stroke='currentColor'>
+                    viewBox='0 0 24 24'>
                     <path
-                      stroke-linecap='round'
-                      stroke-linejoin='round'
-                      stroke-width='2'
-                      d='M4 6h16M4 12h16M4 18h16'
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      strokeWidth='2'
+                      d='M4 6h16M4 12h16M4 18h7'
                     />
                   </svg>
                 </button>
+
+                <Search />
               </div>
- */}
-              <Search />
 
               <Main />
             </div>
--- src/app/tailwind-ui/fields/Search.tsx
+++ src/app/tailwind-ui/fields/Search.tsx
@@ -2,65 +2,48 @@ import React from 'react'
 import { Link } from '../sidebars/DarkSidebarLightHeader/Link'
 
 export const Search = () => (
-  <div className='relative z-10 flex-shrink-0 flex h-16 bg-white shadow'>
-    <button
-      className='px-4 border-r border-gray-200 text-gray-500 focus:outline-none focus:bg-gray-100 focus:text-gray-600 md:hidden'
-      aria-label='Open sidebar'>
-      <svg
-        className='h-6 w-6'
-        stroke='currentColor'
-        fill='none'
-        viewBox='0 0 24 24'>
-        <path
-          stroke-linecap='round'
-          stroke-linejoin='round'
-          stroke-width='2'
-          d='M4 6h16M4 12h16M4 18h7'
-        />
-      </svg>
-    </button>
-    <div className='flex-1 px-4 flex justify-between'>
-      <div className='flex-1 flex'>
-        <div className='w-full flex md:ml-0'>
-          <label htmlFor='search_field' className='sr-only'>
-            Search
-          </label>
-          <div className='relative w-full text-gray-400 focus-within:text-gray-600'>
-            <div className='absolute inset-y-0 left-0 flex items-center pointer-events-none'>
-              <svg className='h-5 w-5' fill='currentColor' viewBox='0 0 20 20'>
-                <path
-                  fill-rule='evenodd'
-                  clip-rule='evenodd'
-                  d='M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z'
-                />
-              </svg>
-            </div>
-            <input
-              id='search_field'
-              className='block w-full h-full pl-8 pr-3 py-2 rounded-md text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 sm:text-sm'
-              placeholder='Search Keybinds (Press the "/" to focus)'
-              type='search'
-            />
+  <div className='flex-1 px-4 flex justify-between'>
+    <div className='flex-1 flex'>
+      <div className='w-full flex md:ml-0'>
+        <label htmlFor='search_field' className='sr-only'>
+          Search
+        </label>
+        <div className='relative w-full text-gray-400 focus-within:text-gray-600'>
+          <div className='absolute inset-y-0 left-0 flex items-center pointer-events-none'>
+            <svg className='h-5 w-5' fill='currentColor' viewBox='0 0 20 20'>
+              <path
+                fillRule='evenodd'
+                clipRule='evenodd'
+                d='M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z'
+              />
+            </svg>
           </div>
+          <input
+            id='search_field'
+            className='block w-full h-full pl-8 pr-3 py-2 rounded-md text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 sm:text-sm'
+            placeholder='Search Keybinds (Press the "/" to focus)'
+            type='search'
+          />
         </div>
       </div>
-      <div className='ml-4 flex items-center md:ml-6'>
-        {/* <!-- Profile dropdown --> */}
-        <div className='ml-3 relative'>
-          <div>
-            <button
-              className='max-w-xs flex items-center text-sm rounded-full focus:outline-none focus:shadow-outline'
-              id='user-menu'
-              aria-label='User menu'
-              aria-haspopup='true'>
-              <img
-                className='h-8 w-8 rounded-full'
-                src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-                alt=''
-              />
-            </button>
-          </div>
-          {/* <!--
+    </div>
+    <div className='ml-4 flex items-center md:ml-6'>
+      {/* <!-- Profile dropdown --> */}
+      <div className='ml-3 relative'>
+        <div>
+          <button
+            className='max-w-xs flex items-center text-sm rounded-full focus:outline-none focus:shadow-outline'
+            id='user-menu'
+            aria-label='User menu'
+            aria-haspopup='true'>
+            <img
+              className='h-8 w-8 rounded-full'
+              src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+              alt=''
+            />
+          </button>
+        </div>
+        {/* <!--
               Profile dropdown panel, show/hide based on dropdown state.
 
               Entering: "transition ease-out duration-100"
@@ -70,16 +53,15 @@ export const Search = () => (
                 From: "transform opacity-100 scale-100"
                 To: "transform opacity-0 scale-95"
             --> */}
-          <div className='origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg'>
-            <div
-              className='py-1 rounded-md bg-white shadow-xs'
-              role='menu'
-              aria-orientation='vertical'
-              aria-labelledby='user-menu'>
-              <Link to='#' title='Your Profile' role='menuitem' />
-              <Link to='#' title='Settings' role='menuitem' />
-              <Link to='#' title='Sign Out' role='menuitem' />
-            </div>
+        <div className='origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg'>
+          <div
+            className='py-1 rounded-md bg-white shadow-xs'
+            role='menu'
+            aria-orientation='vertical'
+            aria-labelledby='user-menu'>
+            <Link to='#' title='Your Profile' role='menuitem' />
+            <Link to='#' title='Settings' role='menuitem' />
+            <Link to='#' title='Sign Out' role='menuitem' />
           </div>
         </div>
       </div>
