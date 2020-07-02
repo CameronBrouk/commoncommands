899637087fc3330e07548e94ce279b5492a3acb5
Cameron Brouk <cameronbrouk@gmail.com>
2020-07-02 00:45:58 -0500

    show list of keybinds

--- src/app/commands/Commands.tsx
+++ src/app/commands/Commands.tsx
@@ -1,18 +1,19 @@
 import React, { useContext } from 'react'
-import { useObservable } from 'rxjs-hooks'
-import { combineLatest } from 'rxjs'
-import { filter } from 'rxjs/operators'
-
-import { useFirestore } from '../shared/hooks'
-import { System, Command } from './models'
 import { CommandsContext } from './Commands.context'
+import { Searchable } from './components'
+
+import { filterCommandsBySystem } from './helpers'
 
 export const Commands = () => {
-  const { commands } = useContext(CommandsContext)
+  const { commands, currentSystem, systems } = useContext(CommandsContext)
+
+  const system = systems.filter(({ name }) => currentSystem === name)[0]
+  // const categorizedCommands = H.sortCommandByCategory(commands)
+  // console.log(categorizedCommands)
 
   return (
-    <div className='text-xl'>
-      <div></div>
+    <div className='text-l'>
+      <Searchable commands={filterCommandsBySystem(commands, system)} />
     </div>
   )
 }
