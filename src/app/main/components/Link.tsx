b02fba5bf67f6bd9034b0abc535346a0dd5956b1
Cameron Brouk <cameronbrouk@gmail.com>
2020-07-02 00:45:11 -0500

    modify selected style

--- src/app/main/components/Link.tsx
+++ src/app/main/components/Link.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { Icon, IconNames } from './Icons/Icon'
+import { IconNames } from './Icons/Icon'
 import { CommandsContext } from 'app/commands/Commands.context'
 
 type Props = {
@@ -13,16 +13,16 @@ export const Link = ({ iconName, title }: Props) => {
   // Show different styles based on whether an icon is given.
   //   NOTE:  this is basically 'sidebar icons' vs 'dropdown icons'
   const classes = iconName
-    ? `group flex items-center leading-5 font-medium text-white bg-gray-800 focus:outline-none focus:bg-gray-700`
+    ? `group flex items-center leading-5 font-medium text-white bg-gray-800 w-4/5 focus:outline-none focus:bg-gray-700`
     : 'block bg-gray-100'
 
-  const isSelected = currentSystem === title ? 'border-b border-white' : ''
+  const isSelected =
+    currentSystem === title ? ' text-yellow-300 bg-indigo-200 rounded-md' : ' '
 
   return (
     <button
-      className={`duration-150 transition ease-in-out text-sm px-4 py-2 ${classes} ${isSelected}`}
+      className={`duration-150 transition ease-in-out text-sm px-2 mx-4 py-2 ${classes} ${isSelected}`}
       onClick={() => switchSystem(title)}>
-      {/* {iconName && <Icon iconName={iconName} />} */}
       {title}
     </button>
   )
