ef44ab4b83336c2f4a9e251a168e0a9aa4fb5c95
Cameron Brouk <cameronbrouk@gmail.com>
2020-07-09 21:17:06 -0500

    add hook to get document from route params

---
+++ src/firebaseHooks/useDocumentFromRouteParams.ts
@@ -0,0 +1,14 @@
+import { useObservable } from 'rxjs-hooks'
+import { useParams } from 'react-router'
+import { CollectionNames } from './firebase.types'
+import { useFirestore } from './useFirestore'
+
+/** Grab Document from the specified collection whos id is equal to the id in route params*/
+export const useDocumentFromRouteParams = (collection: CollectionNames) => {
+  const { getSingle$ } = useFirestore(collection)
+  const { id } = useParams<any>()
+
+  const document = useObservable(() => getSingle$(id))
+
+  return document
+}
--- src/firebaseHooks/useFirestore.ts
+++ src/firebaseHooks/useFirestore.ts
@@ -1,9 +1,7 @@
+import firebase from 'firebase'
 import { docData, collectionData } from 'rxfire/firestore'
 import { Observable } from 'rxjs'
 import { map } from 'rxjs/operators'
-import { useObservable } from 'rxjs-hooks'
-import { useParams } from 'react-router'
-import firebase from 'firebase'
 import { CollectionNames, Document } from './firebase.types'
 
 /**
@@ -49,14 +47,4 @@ export function useFirestore<T>(firestoreCollection: CollectionNames) {
     remove,
     hardDelete,
   }
-}
-
-/** Grab Document from the specified collection whos id is equal to the id in route params*/
-export const useDocumentFromRouteParams = (collection: CollectionNames) => {
-  const { getSingle$ } = useFirestore(collection)
-  const { id } = useParams<any>()
-
-  const document = useObservable(() => getSingle$(id))
-
-  return document
 }
