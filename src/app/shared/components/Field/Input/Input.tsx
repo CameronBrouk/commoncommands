3aa36e7901b1a97c0241e647974727fcaa8a454a
Cameron Brouk <cameronbrouk@gmail.com>
2020-07-17 20:17:38 -0500

    complete Input component functionality

--- src/app/shared/components/Field/Input/Input.tsx
+++ src/app/shared/components/Field/Input/Input.tsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import { UseFormMethods, ValidationRules } from 'react-hook-form'
-import { InputTypes, Validators } from './types'
-import * as H from './helpers'
+import { InputTypes } from './types'
 import { InputError } from './InputError'
 
 type InputProps = {
-  type?: InputTypes
   form: UseFormMethods<Record<string, any>>
   label: string
   name: string
+  type?: InputTypes
+  autoFocus?: boolean
+  autoComplete?: boolean
 }
 
-export const Input: FC<InputProps & Validators> = props => {
-  const { form, label, type, name, ...validators } = props
-  const { register, errors, watch } = form
+type Props = C<InputProps & ValidationRules>
+
+export const Input = ({ form, label, name, ...props }: Props) => {
+  const { register, watch, errors } = form
+  const { type, autoFocus, autoComplete, ...validators } = props
+  const { min, max, minLength, maxLength, required, pattern } = validators
 
   return (
     <label>
@@ -25,8 +29,18 @@ export const Input: FC<InputProps & Validators> = props => {
 
       <input
         id={label}
-        ref={register(H.validatorObject(validators as ValidationRules))}
+        ref={register({
+          ...(min ? { min } : {}),
+          ...(max ? { max } : {}),
+          ...(maxLength ? { maxLength } : {}),
+          ...(minLength ? { minLength } : {}),
+          ...(required ? { required } : {}),
+          ...(pattern ? { pattern } : {}),
+        })}
         type={type ? type : 'text'}
+        name={name}
+        autoFocus={autoFocus}
+        autoComplete={autoComplete ? 'on' : 'off'}
       />
 
       <InputError
@@ -36,40 +50,4 @@ export const Input: FC<InputProps & Validators> = props => {
       />
     </label>
   )
-}
-
-const min = (min?: Pick<Validators, 'min'>) => {
-  if (!min) return {}
-  if (!Array.isArray(min)) return { min: `must be greater than ${min}` }
-  return { min: min[1] }
-}
-
-const max = (max?: Pick<Validators, 'max'>) => {
-  if (!max) return {}
-  if (Array.isArray(max)) return { min: max[1] }
-  return { max: `must be less than ${max}` }
-}
-
-const maxLength = (maxLength?: Pick<Validators, 'maxLength'>) => {
-  if (!maxLength) return {}
-  if (Array.isArray(maxLength)) return { maxLength: maxLength[1] }
-  return { maxLength: `please enter no more than ${maxLength} characters` }
-}
-
-const minLength = (minLength?: Pick<Validators, 'minLength'>) => {
-  if (!minLength) return {}
-  if (Array.isArray(minLength)) return { minLength: minLength[1] }
-  return { minLength: `please enter at least ${minLength} characters` }
-}
-
-const required = (required?: Pick<Validators, 'required'>) => {
-  if (!required) return {}
-  if (Array.isArray(required)) return { required: required[1] }
-  return { required: `required` }
-}
-
-const pattern = (pattern?: Pick<Validators, 'pattern'>) => {
-  if (!pattern) return {}
-  if (Array.isArray(pattern)) return { pattern: pattern[1] }
-  return { pattern: `invalid input` }
 }
--- src/app/shared/components/Field/Input/InputError.tsx
+++ src/app/shared/components/Field/Input/InputError.tsx
@@ -1,55 +1,41 @@
-import * as R from 'ramda'
+import * as U from '../../../utils/common-functions'
 import React from 'react'
-import { FieldError } from 'react-hook-form'
+import { FieldError, ValidationRules } from 'react-hook-form'
 import * as T from './types'
 
 type Props = {
   error: FieldError
   fieldValue: string | number
-  validators: T.Validators
+  validators: ValidationRules
 }
 
-type Types = keyof T.Validators
-
 export const InputError: FC<Props> = ({ error, fieldValue, validators }) => {
-  const isString = (value: any): value is string => typeof value === 'string'
-  const isNumber = (value: any): value is number => typeof value === 'number'
-  const defaultMessages = {
-    minLength:
-      isString(fieldValue) &&
-      isNumber(validators['minLength']) &&
-      `please enter ${
-        validators['minLength'] - fieldValue.length
-      } more characters`,
-    maxLength:
-      typeof fieldValue === 'string' &&
-      typeof validators['maxLength'] === 'number' &&
-      `please enter ${
-        fieldValue.length - validators['maxLength']
-      } less characters`,
-    max:
-      typeof validators['max'] === 'number' &&
-      `please enter a number less than ${validators['max'] + 1}`,
-    min:
-      typeof validators['min'] === 'number' &&
-      `please enter a number more than ${validators['min'] + 1}`,
-    required: 'please fill out this field',
-    pattern: 'invalid input',
-  }
+  const { minLength, maxLength, max, min, required, pattern } = validators
 
-  const hasError = (validatorType: Types) =>
-    error.type && error.type === validatorType
+  const hasError = (validatorType: keyof T.Validators) =>
+    error && error.type && error.type === validatorType
 
-  const message = (type: Types) => {
+  const message = (type: keyof T.Validators) => {
     // @ts-ignore
-    if (Array.isArray(validators[type])) return validators[type][1]
+    if (U.isObj(validators[type])) return validators[type].message
 
-    if (type === 'required') return defaultMessages.required
-    if (type === 'pattern') return defaultMessages.pattern
-    if (type === 'min') return defaultMessages.min
-    if (type === 'max') return defaultMessages.max
-    if (type === 'maxLength') return defaultMessages.maxLength
-    if (type === 'minLength') return defaultMessages.maxLength
+    if (type === 'required') return 'please fill out this field'
+
+    if (type === 'pattern') return 'invalid input'
+
+    if (type === 'min' && min && U.isNum(min))
+      return `please enter a number more than ${min + 1}`
+
+    if (type === 'max' && max && U.isNum(max))
+      return `please enter a number less than ${max + 1}`
+
+    if (U.isNum(fieldValue)) return
+
+    if (type === 'minLength' && minLength && U.isNum(minLength))
+      return `please enter ${minLength - fieldValue.length} more characters`
+
+    if (type === 'maxLength' && maxLength && U.isNum(maxLength))
+      return `please enter ${fieldValue.length - maxLength} less characters`
   }
 
   const E: FC = ({ children }) => <p className='text-red-400'>{children}</p>
