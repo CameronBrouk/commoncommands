e701b8d4dc33209e819f34329fd45eab8da72343
Cameron Brouk <cameronbrouk@gmail.com>
2020-07-18 14:32:07 -0500

    add Image component

--- src/app/shared/components/Media/Image.tsx
+++ src/app/shared/components/Media/Image.tsx
@@ -1,3 +1,32 @@
 import React from 'react'
 
-export const Image: FC<{}> = (props) => <div></div>
+export type Props = {
+  src: string
+  alt: string
+  fit: 'contain' | 'cover'
+  overlay?: boolean
+}
+
+/**
+ * @param src name of image(with suffix)
+ * @param alt image alt text
+ * @param fit object-fit css property. contain or cover;
+ */
+export const Image = ({ src, alt, fit, ...props }: C<Props>) => {
+  const sharedClasses = `absolute top-0 left-0 w-auto h-auto max-w-full max-h-full`
+
+  return (
+    <section className={`relative overflow-hidden ${props.className}`}>
+      <img
+        src={require(`../../../../assets/${src}`)}
+        alt={alt}
+        className={`object-${fit} z-10 ${sharedClasses}`}
+      />
+
+      <div className={`z-20 ${sharedClasses}`}>
+        {/* This Content will overlayed on top of the image */}
+        {props.children}
+      </div>
+    </section>
+  )
+}
