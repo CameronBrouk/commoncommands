a40a8518070a91d49bb7ffe2a68a4b216b281931
Cameron Brouk <cameronbrouk@gmail.com>
2020-07-18 17:27:38 -0500

    move generic firestore types to types file

--- src/app/firebase/firestore/firestore.types.ts
+++ src/app/firebase/firestore/firestore.types.ts
@@ -11,3 +11,16 @@ export type Document<T> = T & {
   createdAt: string
   updatedAt: string
 }
+
+export type Keys<T> = keyof T extends string ? keyof T : string
+export type FirestoreWhere<T> = [Keys<T>, firebase.firestore.WhereFilterOp, any]
+export type QueryParams<T> = {
+  where?: FirestoreWhere<T>[]
+  orderBy?: keyof T
+  limit?: number
+}
+
+export type DocData = firebase.firestore.DocumentData
+export type CollectionRef = firebase.firestore.CollectionReference<DocData>
+export type FirestoreQuery = firebase.firestore.Query<DocData>
+export type Query = FirestoreQuery | CollectionRef
