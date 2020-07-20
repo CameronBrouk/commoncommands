3c0c8394a77a14b79f1ce582cc7f7c73fdbe383d
Cameron Brouk <cameronbrouk@gmail.com>
2020-07-20 15:27:00 -0500

    remove route related functions from useFirestore

--- src/app/firebase/firestore/useFirestore.ts
+++ src/app/firebase/firestore/useFirestore.ts
@@ -2,7 +2,6 @@ import firebase from 'firebase'
 import { docData, collectionData } from 'rxfire/firestore'
 import { map } from 'rxjs/operators'
 import { CollectionNames, Document } from './firestore.types'
-import { useRouter } from 'app/shared/hooks'
 
 /**
  * This hook is the single source of truth for API requests to the firestore database in this application, and is meant to be extended
@@ -10,12 +9,10 @@ import { useRouter } from 'app/shared/hooks'
  * @param firestoreCollection name of the collection in firebase firestore
  */
 export function useFirestore<T>(firestoreCollection: CollectionNames) {
-  const { routeParams } = useRouter<{ id: string }>()
   // Type Declarations
   type Doc = Document<T>
 
   // shared / single use functions
-  const { id: routeParamId } = routeParams
   const collection = firebase.firestore().collection(firestoreCollection)
   const getDocument = (id: string) => collection.doc(id)
   const filterDeleted = (data: Doc[]) => data.filter(({ deleted }) => !deleted)
@@ -26,9 +23,6 @@ export function useFirestore<T>(firestoreCollection: CollectionNames) {
 
   /** Get a Single Document from Firestore */
   const getSingle$ = (id: string) => docData<Doc>(collection.doc(id))
-
-  /** Get Id from route params and use it to get a document with that id*/
-  const getFromRouteParams$ = () => docData<Doc>(collection.doc(routeParamId))
 
   /** Create a new record */
   const create = async (id: string, data: T) =>
@@ -49,7 +43,6 @@ export function useFirestore<T>(firestoreCollection: CollectionNames) {
     collection,
     list$,
     getSingle$,
-    getFromRouteParams$,
     create,
     update,
     remove,
