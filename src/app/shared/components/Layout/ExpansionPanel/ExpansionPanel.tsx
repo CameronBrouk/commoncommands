8e1b20bbc3d24111dd57f1832e2623493f8fa354
Cameron Brouk <cameronbrouk@gmail.com>
2020-07-31 20:10:35 -0500

    fix duplicate expansion panel issue

--- src/app/shared/components/Layout/ExpansionPanel/ExpansionPanel.tsx
+++ src/app/shared/components/Layout/ExpansionPanel/ExpansionPanel.tsx
@@ -6,13 +6,11 @@ type Props = { title: string }
 export const ExpansionPanel: FC<Props> = ({ children, title }) => {
   return (
     <>
-      {Children.map(children, (child, i) => (
-        <div className='max-w-screen-xl mx-auto sm:pt-2 px-4'>
-          <div className='pb-2 border-b-2 border-gray-200 pt-3 pb-5 transition ease-in'>
-            <PanelContent title={title}>{child}</PanelContent>
-          </div>
+      <div className='max-w-screen-xl mx-auto sm:pt-2 px-4'>
+        <div className='pb-2 border-b-2 border-gray-200 pt-3 pb-5 transition ease-in'>
+          <PanelContent title={title}>{children}</PanelContent>
         </div>
-      ))}
+      </div>
     </>
   )
 }
