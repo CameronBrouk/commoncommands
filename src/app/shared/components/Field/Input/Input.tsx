ad25fd30f285dff3279beebfd811a31f6330a7fc
Cameron Brouk <cameronbrouk@gmail.com>
2020-08-10 01:05:32 -0500

    add styles to Input Field

--- src/app/shared/components/Field/Input/Input.tsx
+++ src/app/shared/components/Field/Input/Input.tsx
@@ -21,34 +21,41 @@ export const Input = ({ form, label, name, ...props }: Props) => {
   const { min, max, minLength, maxLength, required, pattern } = validators
 
   return (
-    <label className={props.className || '' + 'flex flex-col'}>
-      <span>
-        <span>{validators.required && '*'}</span>
-        {label}
-        <span>{!validators.required && '(optional)'}</span>
-      </span>
+    <div className={props.className || 'mt-6'}>
+      <label
+        className='text-sm font-medium leading-5 text-gray-700 lock'
+        htmlFor={label}>
+        <span>
+          <span>{validators.required && '*'}</span>
+          {label}
+          <span>{!validators.required && '(optional)'}</span>
+        </span>
+      </label>
 
-      <input
-        id={label}
-        ref={register({
-          ...(min ? { min } : {}),
-          ...(max ? { max } : {}),
-          ...(maxLength ? { maxLength } : {}),
-          ...(minLength ? { minLength } : {}),
-          ...(required ? { required } : {}),
-          ...(pattern ? { pattern } : {}),
-        })}
-        type={type ? type : 'text'}
-        name={name}
-        autoFocus={autoFocus}
-        autoComplete={autoComplete ? 'on' : 'off'}
-      />
+      <div className='mt-1 rounded-md shadow-sm'>
+        <input
+          className='block w-full px-3 py-2 placeholder-gray-400 transition duration-150 ease-in-out border border-gray-300 rounded-md appearance-none focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5'
+          id={label}
+          ref={register({
+            ...(min ? { min } : {}),
+            ...(max ? { max } : {}),
+            ...(maxLength ? { maxLength } : {}),
+            ...(minLength ? { minLength } : {}),
+            ...(required ? { required } : {}),
+            ...(pattern ? { pattern } : {}),
+          })}
+          type={type ? type : 'text'}
+          name={name}
+          autoFocus={autoFocus}
+          autoComplete={autoComplete ? 'on' : 'off'}
+        />
+      </div>
 
       <InputError
         fieldValue={watch(name)}
         error={errors[name]}
         validators={validators}
       />
-    </label>
+    </div>
   )
 }
