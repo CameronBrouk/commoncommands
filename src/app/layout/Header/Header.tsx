2ad637f3efc958900980e2c555d8808d93bae949
Cameron Brouk <cameronbrouk@gmail.com>
2020-08-09 20:55:45 -0500

    move profile dropdown button to the right

--- src/app/layout/Header/Header.tsx
+++ src/app/layout/Header/Header.tsx
@@ -7,9 +7,9 @@ type Props = {
 }
 export const Header = ({ openSidebar }: Props) => {
   return (
-    <div className='relative z-10 flex-shrink-0 flex h-16 bg-white shadow'>
+    <div className='relative z-10 flex flex-shrink-0 h-16 bg-white shadow'>
       <OpenSidebarButton openSidebar={openSidebar} />
-      <div className='flex-1 px-4 flex justify-between'>
+      <div className='flex justify-end flex-1 px-4'>
         <ProfileDropdown />
       </div>
     </div>
