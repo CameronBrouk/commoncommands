9d89fd1a2f0460e064658ac0093b882b0e929990
Cameron Brouk <cameronbrouk@gmail.com>
2020-08-09 20:51:28 -0500

    add Register functionality

--- src/app/auth/Register.tsx
+++ src/app/auth/Register.tsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import firebase from 'firebase'
 import { useForm } from 'react-hook-form'
-import { Permissions, useFirestore } from '../firebase'
+import { Permissions, useFirestore, User } from '../firebase'
+import { Input } from 'app/shared/components'
+import { useRouter } from 'app/shared/hooks'
 
 type Props = {
   afterRegister?: (user: any) => void
 }
-
-export const Register: FC<Props> = props => {
+export const Register = ({ afterRegister }: Props) => {
+  const [error, setError] = useState('')
   const { create: createPermissions } = useFirestore<Permissions>('permissions')
-  const { register, handleSubmit, watch, errors } = useForm()
+  const { create: createUser } = useFirestore<User>('users')
+  const { navigateTo } = useRouter()
+  const formProps = useForm()
   const auth = firebase.auth()
 
-  const onSubmit = (formData: any) => {
+  type FormData = {
+    email: string
+    password: string
+  }
+  const onSubmit = (formData: FormData) => {
     auth
       .createUserWithEmailAndPassword(formData.email, formData.password)
       .then(({ user }) => {
@@ -23,23 +31,59 @@ export const Register: FC<Props> = props => {
             groups: ['user'],
           })
 
-          !!props.afterRegister && props.afterRegister(user)
+          createUser(user.uid, {
+            email: user.email || '',
+            emailVerified: user.emailVerified,
+            uid: user.uid,
+            displayName: user.displayName || '',
+            phone: user.phoneNumber || '',
+            photoUrl: user.photoURL || '',
+          })
+
+          if (!!afterRegister) afterRegister(user)
         }
       })
-      .catch(console.log)
+      .catch(({ message }) => setError(message))
   }
 
   return (
-    <div className={props.className}>
-      <h1>Register</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
+    <div>
+      <h1 className='text-xl'>Register</h1>
+
+      <form
+        onSubmit={formProps.handleSubmit<FormData>(onSubmit)}
+        className='flex flex-col'>
+        <Input
+          className='flex m-1'
+          form={formProps}
+          label='Email'
+          type='email'
+          name='email'
+          pattern={{
+            value: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}/,
+            message: 'Invalid Email',
+          }}
+        />
+
+        <Input
+          className='flex m-1'
+          form={formProps}
+          label='Password'
+          type='password'
+          name='password'
+        />
+
+        {error && <p className='text-red-400'>{error}</p>}
+
         <div className='buttons'>
-          {/* <Button type='submit' color='primary' variant='contained'>
-            Register
-          </Button> */}
-          {/* <Button type='button' onClick={() => history.push('/login')}>
+          <button className='m-1 btn-blue' type='submit'>
             Login
-          </Button> */}
+          </button>
+          <button
+            className='m-1 btn-blue'
+            onClick={() => navigateTo('/register')}>
+            Register
+          </button>
         </div>
       </form>
     </div>
