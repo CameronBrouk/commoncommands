201b89688c1a77d1392af66c5793e3ade217e0ba
Cameron Brouk <cameronbrouk@gmail.com>
2020-11-13 15:41:01 -0600

    add Redirect component

---
+++ src/app/QRCodeGenerator/Redirect.tsx
@@ -0,0 +1,27 @@
+import { useFirestore, useRouter } from 'app/shared/hooks'
+import { tap, first } from 'rxjs/operators'
+import React from 'react'
+import { Code } from './QRCode.types'
+import { useObservable } from 'rxjs-hooks'
+
+export const Redirect = () => {
+  const { routeParams } = useRouter<{ id: string }>()
+  const { getSingle$, update } = useFirestore<Code>('codes')
+
+  useObservable(() =>
+    getSingle$(routeParams.id).pipe(
+      tap(({ impressions, url }) =>
+        update(routeParams.id, { impressions: impressions + 1 }).then(() => {
+          document.location.replace('https://' + url)
+        }),
+      ),
+      first(),
+    ),
+  )
+
+  return (
+    <div className='flex justify-center'>
+      <img src={require('../../assets/loading-spinner.svg')} />
+    </div>
+  )
+}
