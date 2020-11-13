4b80d5a3639dd4f3e78d6101b27f3352d6de66d5
Cameron Brouk <cameronbrouk@gmail.com>
2020-11-13 15:43:58 -0600

    add Routing to the application

--- src/app/Routes.tsx
+++ src/app/Routes.tsx
@@ -1,13 +1,39 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import { Login, Register } from './auth'
+import { usePermissions } from './firebase'
+import { AwaitingApproval } from './QRCodeGenerator/AwaitingApproval'
+import { Dashboard } from './QRCodeGenerator/Dashboard'
+import { Home } from './QRCodeGenerator/Home'
+import { QRCodeForm } from './QRCodeGenerator/QRCodeForm'
+import { Redirect } from './QRCodeGenerator/Redirect'
+import { Users } from './users/Users'
 
 export default function AppRoutes() {
+  const { isLoggedIn, hasRole } = usePermissions()
+
   return (
     <Switch>
-      <Route path='/login'>{/* <Login /> */}</Route>
-      <Route path='/register'>{/* <Register /> */}</Route>
-      <Route path='/'>{/* <Home /> */}</Route>
+      <Route path='/create-qr'>
+        {isLoggedIn() ? <QRCodeForm /> : <Home />}
+      </Route>
+
+      <Route path='/dashboard'>{isLoggedIn() ? <Dashboard /> : <Home />}</Route>
+
+      <Route path='/users'>
+        {hasRole('admin') ? <Users /> : <Dashboard />}
+      </Route>
+
+      <Route path='/redirect/:id'>
+        <Redirect />
+      </Route>
+
+      <Route path='/awaiting-approval'>
+        <AwaitingApproval />
+      </Route>
+
+      <Route path='/'>
+        <Home />
+      </Route>
     </Switch>
   )
 }
