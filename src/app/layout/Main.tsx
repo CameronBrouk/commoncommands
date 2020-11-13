5fc7a645b28fda07dea784144da1e8357a62e13a
Cameron Brouk <cameronbrouk@gmail.com>
2020-11-13 15:37:48 -0600

    clean up <Main /> component

--- src/app/layout/Main.tsx
+++ src/app/layout/Main.tsx
@@ -3,15 +3,16 @@ import React, { useContext } from 'react'
 export const Main: FC<{}> = ({ children }) => {
   return (
     <main
-      className='flex-1 relative z-0 overflow-y-auto pt-2 pb-6 focus:outline-none md:py-6'
+      className='relative z-0 flex-1 pt-2 pb-6 overflow-y-auto focus:outline-none md:py-6'
       tabIndex={0}>
-      <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+      <div className='px-4 mx-auto max-w-7xl sm:px-6 lg:px-8'>
         <h1 className='text-2xl font-semibold text-gray-900 sm:text-center md:text-left'>
-          Page Title
+          {/* Build and Customize a QR Code */}
         </h1>
       </div>
-      <div className='max-w-7xl mx-auto md:px-2'>
-        <div className='py-4'>{children}</div>
+      <div className='mx-auto max-w-7xl md:px-2'>
+        {/* <div className='py-2'>{children}</div> */}
+        {children}
       </div>
     </main>
   )
