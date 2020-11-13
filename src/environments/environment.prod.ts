8693dc98c4a4f590971b38f6325d14ad19fc0424
Cameron Brouk <cameronbrouk@gmail.com>
2020-11-13 15:40:39 -0600

    update environment to correct firebase project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const prodEnvironment = {
   production: true,
   firebaseConfig: {
-    apiKey: 'AIzaSyCxVz4-yK-au5JY3RW2KyNHM_s6dPRK0pY',
-    authDomain: 'common-commands.firebaseapp.com',
-    databaseURL: 'https://common-commands.firebaseio.com',
-    projectId: 'common-commands',
-    storageBucket: 'common-commands.appspot.com',
-    messagingSenderId: '692956401158',
-    appId: '1:692956401158:web:42546e429b3595dac93d87',
-    measurementId: 'G-20WRPLNY5G',
+    apiKey: 'AIzaSyAUr3uq101J58QcjsbeVQmS_-htKrn03-w',
+    authDomain: 'alexander-qr.firebaseapp.com',
+    databaseURL: 'https://alexander-qr.firebaseio.com',
+    projectId: 'alexander-qr',
+    storageBucket: 'alexander-qr.appspot.com',
+    messagingSenderId: '449085635889',
+    appId: '1:449085635889:web:7b6474b92a923e1cd70410',
+    measurementId: 'G-VRFBEGB2XM',
   },
 }
 
