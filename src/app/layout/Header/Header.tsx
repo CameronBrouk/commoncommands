bbdf8e328e03ca06d44815bdbc7c902b475feae1
Cameron Brouk <cameronbrouk@gmail.com>
2021-02-04 17:00:19 -0600

    add sign in modal to header

--- src/app/layout/Header/Header.tsx
+++ src/app/layout/Header/Header.tsx
@@ -1,20 +1,31 @@
-import React from 'react'
-import { ProfileDropdown } from './ProfileDropdown'
+import React, { useState } from 'react'
 import { OpenSidebarButton } from './OpenSidebarButton'
-import { Search } from 'app/shared/components'
+import { Button, Search } from 'app/shared/components'
+import { SignInModal } from 'app/auth'
 
 type Props = {
   openSidebar: () => void
   onSearch: (input: React.ChangeEvent<HTMLInputElement>) => void
 }
+
 export const Header = ({ openSidebar, onSearch }: Props) => {
+  const [loginVisible, setLoginVisible] = useState(false)
   return (
     <div className='relative z-10 flex flex-shrink-0 h-16 bg-white shadow'>
       <OpenSidebarButton openSidebar={openSidebar} />
       <div className='flex justify-between flex-1 px-4'>
         <Search onSearch={onSearch} />
-        <ProfileDropdown />
+        <Button
+          variant='raised'
+          className='m-3'
+          onClick={() => setLoginVisible(v => !v)}>
+          Login
+        </Button>
       </div>
+      <SignInModal
+        isVisible={loginVisible}
+        onClose={() => setLoginVisible(false)}
+      />
     </div>
   )
 }
